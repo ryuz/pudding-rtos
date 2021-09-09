cd6f0ca5a0ce3aa492bf77eb235e69fd7efd8770
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-09-10 07:13:39 +0900

    update

--- src/context.rs
+++ src/context.rs
@@ -8,36 +8,34 @@ pub struct ContextControlBlock {
 }
 
 extern "C" {
+    // コンテキスト生成
     fn _kernel_context_create(
         ctxcb: *mut ContextControlBlock,
         isp: usize,
         entry: extern "C" fn(isize),
-        ext: isize,
+        exinf: isize,
     );
 
-    fn _kernel_context_start(
-        ctxcb_new: *mut ContextControlBlock,
-    );
+    // コンテキスト開始
+    fn _kernel_context_start(ctxcb_new: *mut ContextControlBlock);
 
+    // コンテキストスイッチ
     fn _kernel_context_switch(
-        ctxcb_new: *mut ContextControlBlock,
-        ctxcb_now: *mut ContextControlBlock,
+        ctxcb_next: *mut ContextControlBlock,
+        ctxcb_current: *mut ContextControlBlock,
     );
 }
 
 
 impl ContextControlBlock {
-    pub fn new(stack: &mut [isize], entry: extern "C" fn(isize), ext: isize) -> Self {
-        let isp = (&stack[0] as *const isize as usize) + stack.len() * size_of::<isize>();
-        //      let mut ctxcb = ContextControlBlock {sp : 0};
-        let mut ctxcb: ContextControlBlock = Default::default();
-        ctxcb.create(isp, entry, ext);
-        ctxcb
+    pub fn new() -> Self {
+        ContextControlBlock {sp : 0}
     }
 
-    pub fn create(&mut self, isp: usize, entry: extern "C" fn(isize), ext: isize) {
+    pub fn create(&mut self, stack: &mut [isize], entry: extern "C" fn(isize), exinf: isize) {
+        let isp = (&stack[0] as *const isize as usize) + stack.len() * size_of::<isize>();
         unsafe {
-            _kernel_context_create(self, isp as usize, entry, ext);
+            _kernel_context_create(self, isp as usize, entry, exinf);
         }
     }
 
@@ -47,9 +45,9 @@ impl ContextControlBlock {
         }
     }
 
-    pub fn switch(&mut self, ctxcb_now: &mut ContextControlBlock) {
+    pub fn switch(&mut self, ctxcb_next: &mut ContextControlBlock) {
         unsafe {
-            _kernel_context_switch(ctxcb_now, self);
+            _kernel_context_switch(ctxcb_next, self);
         }
     }
 }
--- src/task.rs
+++ src/task.rs
@@ -1,14 +1,17 @@
-
-
-
-use core::ptr;
 use core::marker::PhantomData;
+use core::ptr;
 
+use super::context::*;
+
+type Priority = i32;
 
 pub struct TaskControlBlock<'a> {
+    context: ContextControlBlock,
     queue: *mut TaskQueue<'a>,
     next: *mut TaskControlBlock<'a>,
-    priority: i32,
+    priority: Priority,
+    task: fn(isize),
+    exinf: isize,
     marker: PhantomData<&'a TaskControlBlock<'a>>,
 }
 
@@ -16,16 +19,30 @@ pub struct TaskQueue<'a> {
     tail: *mut TaskControlBlock<'a>,
 }
 
+extern "C" fn task_entry(exinf: isize)
+{
+    let task_ptr = exinf as *mut TaskControlBlock;
+    let task = unsafe { &mut *task_ptr };
+    (task.task)(task.exinf);
+}
+
 impl<'a> TaskControlBlock<'a> {
-    pub fn new() -> Self {
-        TaskControlBlock {
-            queue: ptr::null_mut(), 
+    pub fn new(exinf: isize, task: fn(isize), priority: Priority, stack: &mut [isize]) -> Self {
+        let mut task = TaskControlBlock {
+            context: ContextControlBlock::new(),
+            queue: ptr::null_mut(),
             next: ptr::null_mut(),
-            priority: 0,
+            priority: priority,
+            task: task,
+            exinf: exinf,
             marker: PhantomData,
-        }
+        };
+        let task_ptr = &mut task as *mut TaskControlBlock;
+        task.context.create(stack, task_entry, task_ptr as isize);
+        task
     }
 }
+
 
 impl<'a> TaskQueue<'a> {
     pub fn new() -> Self {
@@ -35,48 +52,48 @@ impl<'a> TaskQueue<'a> {
     }
 
     /// 優先度順で追加
-    pub fn insert_priority_order(&mut self, task: &'a mut TaskControlBlock<'a>)
-    {
+    pub fn insert_priority_order(&mut self, task: &'a mut TaskControlBlock<'a>) {
         // タスクに所属キューを設定
         task.queue = self as *mut TaskQueue<'a>;
 
         // 生ポインタ化
-        let task_ptr:*mut TaskControlBlock<'a> = task as *mut TaskControlBlock<'a>;
+        let task_ptr: *mut TaskControlBlock<'a> = task as *mut TaskControlBlock<'a>;
 
-        if self.tail == ptr::null_mut() { // キューにタスクが無ければ先頭に設定
+        if self.tail == ptr::null_mut() {
+            // キューにタスクが無ければ先頭に設定
             task.next = task_ptr;
             self.tail = task_ptr;
-        }
-        else { // キューが空でないなら挿入位置を探索
+        } else {
+            // キューが空でないなら挿入位置を探索
             // タスク優先度を取得
             let task_pri = task.priority;
-            
+
             // 先頭から探索
-            let mut prev_ptr  = self.tail;
-            let mut prev_task = unsafe{ &mut *prev_ptr };
-            let mut next_ptr  = prev_task.next;
-            let mut next_task = unsafe{ &mut *next_ptr };
+            let mut prev_ptr = self.tail;
+            let mut prev_task = unsafe { &mut *prev_ptr };
+            let mut next_ptr = prev_task.next;
+            let mut next_task = unsafe { &mut *next_ptr };
             loop {
                 // 優先度取り出し
                 let next_pri = next_task.priority;
-                
+
                 if next_pri > task_pri {
                     break;
                 }
-                
+
                 // 次を探す
-                prev_ptr  = next_ptr;
+                prev_ptr = next_ptr;
                 prev_task = next_task;
-                next_ptr  = prev_task.next;
-                next_task = unsafe{ &mut *next_ptr };
-                
+                next_ptr = prev_task.next;
+                next_task = unsafe { &mut *next_ptr };
+
                 // 末尾なら抜ける
                 if prev_ptr == self.tail {
                     self.tail = task as *mut TaskControlBlock;
                     break;
                 }
             }
-            
+
             // 挿入
             prev_task.next = task as *mut TaskControlBlock;
             task.next = next_ptr;
@@ -87,45 +104,43 @@ impl<'a> TaskQueue<'a> {
     pub fn push_back(&mut self, task: &'a mut TaskControlBlock<'a>) {
         // 生ポインタ化
         let task_ptr = task as *mut TaskControlBlock;
-        
+
         // タスクに所属キューを設定
         task.queue = self as *mut TaskQueue;
 
-        if self.tail == ptr::null_mut() { // キューにタスクが無ければ先頭に設定
+        if self.tail == ptr::null_mut() {
+            // キューにタスクが無ければ先頭に設定
             task.next = task_ptr;
-        }
-        else // キューが空でないなら末尾に追加
+        } else
+        // キューが空でないなら末尾に追加
         {
-            let tail_task = unsafe{ &mut *self.tail };
+            let tail_task = unsafe { &mut *self.tail };
             task.next = tail_task.next;
             tail_task.next = task_ptr;
         }
         self.tail = task_ptr;
     }
 
-    pub fn front(&mut self) -> Option<&'a mut TaskControlBlock<'a>>
-    {
+    /// 先頭を参照
+    pub fn front(&mut self) -> Option<&'a mut TaskControlBlock<'a>> {
         if self.tail == ptr::null_mut() {
             None
-        }
-        else {
-            let task_tail = unsafe{&mut *self.tail};
-            Some(unsafe{&mut *task_tail.next})
+        } else {
+            let task_tail = unsafe { &mut *self.tail };
+            Some(unsafe { &mut *task_tail.next })
         }
     }
 
-    pub fn pop_front(&mut self) -> Option<&'a mut TaskControlBlock<'a>>
-    {
+    /// 先頭を取り出し
+    pub fn pop_front(&mut self) -> Option<&'a mut TaskControlBlock<'a>> {
         if self.tail == ptr::null_mut() {
             None
-        }
-        else {
-            let task_tail = unsafe{&mut *self.tail};
-            let task_head = unsafe{&mut *task_tail.next};
+        } else {
+            let task_tail = unsafe { &mut *self.tail };
+            let task_head = unsafe { &mut *task_tail.next };
             if self.tail == task_tail.next {
                 self.tail = ptr::null_mut();
-            }
-            else {
+            } else {
                 task_tail.next = task_head.next;
             }
             Some(task_head)
@@ -133,42 +148,29 @@ impl<'a> TaskQueue<'a> {
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    
-    static mut QUE:TaskQueue = TaskQueue { tail:ptr::null_mut() };
-    static mut TASK0:TaskControlBlock = TaskControlBlock { queue:ptr::null_mut(), next:ptr::null_mut(), priority:0, marker:PhantomData, };
-    static mut TASK1:TaskControlBlock = TaskControlBlock { queue:ptr::null_mut(), next:ptr::null_mut(), priority:1, marker:PhantomData, };
 
     #[test]
-    fn task_queue_test1() {
-        let value = 10;
-        assert_eq!(10, value);
-
+    fn test_task_queue() {
         unsafe {
-            QUE.push_back(&mut TASK0);
-            QUE.push_back(&mut TASK1);
-            let t0 = QUE.pop_front();
-            let t1 = QUE.pop_front();
+            let mut que: TaskQueue = TaskQueue::new();
+            static mut STACK0: [isize; 256] = [0; 256];
+            static mut STACK1: [isize; 256] = [0; 256];
+            let mut task0: TaskControlBlock = TaskControlBlock::new(&mut STACK0, task0, 0, 0);
+            let mut task1: TaskControlBlock = TaskControlBlock::new(&mut STACK1, task1, 1, 0);
+            que.push_back(&mut task0);
+            que.push_back(&mut task1);
+            let t0 = que.pop_front();
+            let t1 = que.pop_front();
+            let t2 = que.pop_front();
             assert_eq!(t0.unwrap().priority, 0);
             assert_eq!(t1.unwrap().priority, 1);
+            assert_eq!(t2.is_some(), false);
         }
     }
 
-    #[test]
-    fn task_queue_test2() {
-        let mut que:TaskQueue = TaskQueue { tail:ptr::null_mut() };
-        let mut task0:TaskControlBlock = TaskControlBlock { queue:ptr::null_mut(), next:ptr::null_mut(), priority:0, marker:PhantomData };
-        let mut task1:TaskControlBlock = TaskControlBlock { queue:ptr::null_mut(), next:ptr::null_mut(), priority:1, marker:PhantomData };
-        que.push_back(&mut task0);
-        que.push_back(&mut task1);
-        let t0 = que.pop_front().unwrap();
-        let t1 = que.pop_front().unwrap();
-        assert_eq!(t0.priority, 0);
-        assert_eq!(t1.priority, 1);
-    }
+    fn task0(_ext: isize) {}
+    fn task1(_ext: isize) {}
 }
-
