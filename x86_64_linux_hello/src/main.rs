c645f4eb6a97c6f7024a29a6308feff8f3a5202d
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-09-10 20:59:06 +0900

    update

---
+++ x86_64_linux_hello/src/main.rs
@@ -0,0 +1,34 @@
+
+//#![no_std]
+//#![no_main]
+//#![feature(asm)]
+
+
+use kernel::task::*;
+
+fn main()
+{
+    println!("Hello!");
+
+    
+    unsafe {
+        let mut que: TaskQueue = TaskQueue::new();
+        static mut STACK0: [isize; 256] = [0; 256];
+        static mut STACK1: [isize; 256] = [0; 256];
+        let mut task0: TaskControlBlock = TaskControlBlock::new(0, task0,0, &mut STACK0);
+        let mut task1: TaskControlBlock = TaskControlBlock::new(0, task1,1, &mut STACK1);
+        que.push_back(&mut task0);
+        que.push_back(&mut task1);
+        let t0 = que.pop_front();
+        let t1 = que.pop_front();
+        let t2 = que.pop_front();
+//        assert_eq!(t0.unwrap().priority, 0);
+//        assert_eq!(t1.unwrap().priority, 1);
+//        assert_eq!(t2.is_some(), false);
+    }
+    
+}
+
+
+fn task0(_ext: isize) {}
+fn task1(_ext: isize) {}
