0fe4f011e0ae33c6c3ce9a29ad67a195f68ba8ea
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-09-11 18:34:07 +0900

    update

--- x86_64_linux_hello/src/main.rs
+++ x86_64_linux_hello/src/main.rs
@@ -1,44 +1,18 @@
-//#![no_std]
-//#![no_main]
-//#![feature(asm)]
+
 
 #[macro_use]
 extern crate kernel;
 
 use kernel::context::*;
 use kernel::task::*;
-use core::ptr;
-
-//use once_cell::sync::Lazy;
-//use lazy_static::lazy_static;
-
 
 
 static mut STACK0: [isize; 256] = [0; 256];
-//static mut TASK0: Lazy<Task> = Lazy::new(|| Task::new(0, task0, 0, unsafe { &mut STACK0 }));
 static mut TASK0: Task = task_default!();
 
 static mut STACK1: [isize; 256] = [0; 256];
 static mut TASK1: Task = task_default!();
 
-
-/*
-lazy_static! {
-    static ref STACK0: [isize; 256] = [0; 256];
-//    static ref  TASK0: Task = Task::new(0, task0, 0, unsafe { &mut STACK0 });
-    
-    static ref STACK1: [isize; 256] = [0; 256];
-//    static ref  TASK1: Task = Task::new(0, task1, 1, unsafe { &mut STACK1 });
-}
-*/
-
-fn task0(_ext: isize) {
-    println!("Task0");
-}
-
-fn task1(_ext: isize) {
-    println!("Task1");
-}
 
 fn main() {
     println!("Start");
@@ -48,39 +22,18 @@ fn main() {
         TASK0.create(0, task0, 0, &mut STACK0);
         TASK1.create(0, task1, 0, &mut STACK1);
 
-        let pri = TASK0.get_priority();
-        println!("{}", pri);
-
-//        let task1: Task = Task::new(0, task1, 1, &mut STACK1);
-
         TASK0.activate();
         TASK1.activate();
-//      TASK0.activate();
-//        TASK1.activate();
     }
     println!("End");
 }
 
-/*
-fn main() {
-    unsafe {
-        println!("Hello!");
 
-        static mut QUE: Lazy<TaskQueue> = Lazy::new(|| TaskQueue::new());
-        static mut STACK0: [isize; 256] = [0; 256];
-        static mut STACK1: [isize; 256] = [0; 256];
-        static mut TASK0: Lazy<Task> = Lazy::new(|| Task::new(0, task0, 0, unsafe { &mut STACK0 }));
-        static mut TASK1: Lazy<Task> = Lazy::new(|| Task::new(0, task1, 1, unsafe { &mut STACK1 }));
-        QUE.push_back(&mut TASK0);
-        QUE.push_back(&mut TASK1);
-        let t0 = QUE.pop_front();
-        let t1 = QUE.pop_front();
-        let t2 = QUE.pop_front();
-        assert_eq!(t0.unwrap().get_priority(), 0);
-        assert_eq!(t1.unwrap().get_priority(), 1);
-        assert_eq!(t2.is_some(), false);
-
-        TASK0.activate();
-    }
+fn task0(_exinf: isize) {
+    println!("Task0");
 }
-*/
+
+fn task1(_exinf: isize) {
+    println!("Task1");
+}
+
--- zynqmp_rpu_hello/src/main.rs
+++ zynqmp_rpu_hello/src/main.rs
@@ -24,7 +24,12 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
 }
 
 
+#[macro_use]
+extern crate kernel;
+
+use kernel::context::*;
 use kernel::task::*;
+
 
 // main
 #[no_mangle]
@@ -32,40 +37,35 @@ pub unsafe extern "C" fn main() -> ! {
     wait(10000);
     println!("Hello! world!");
     
+    println!("Start");
     {
-        let mut que: TaskQueue = TaskQueue::new();
+        kernel::initialize();
+
         static mut STACK0: [isize; 256] = [0; 256];
         static mut STACK1: [isize; 256] = [0; 256];
-        let mut task0: TaskControlBlock = TaskControlBlock::new(0, task0,0, &mut STACK0);
-        let mut task1: TaskControlBlock = TaskControlBlock::new(0, task1,1, &mut STACK1);
-        que.push_back(&mut task0);
-        que.push_back(&mut task1);
-        let t0 = que.pop_front();
-        let t1 = que.pop_front();
-        let t2 = que.pop_front();
-        assert_eq!(t0.unwrap().get_priority(), 0);
-        assert_eq!(t1.unwrap().get_priority(), 1);
-        assert_eq!(t2.is_some(), false);
+        static mut TASK0: Task = task_default!();
+        static mut TASK1: Task = task_default!();
+
+        TASK0.create(0, task0, 0, &mut STACK0);
+        TASK1.create(1, task1, 1, &mut STACK1);
+        TASK0.activate();
+        TASK1.activate();
     }
+    println!("End");
 
     loop {}
 }
 
 
 
-fn task0(_ext:isize)
+fn task0(_exinf:isize)
 {
-    loop {
-        wait(1000);
-        println!("Task0");
-    }
+    println!("Task0");
 }
 
-fn task1(_ext:isize)
+fn task1(_exinf:isize)
 {
-    loop {
-        wait(1000);
-        println!("Task1");
-    }
+    println!("Task1");
 }
+
 
