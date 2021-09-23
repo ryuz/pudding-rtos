a49e7465c7ba4578bc7ebe2ce046a858c27df738
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-09-23 18:17:21 +0900

    update

--- zynqmp_rpu_no_kernel/src/timer.rs
+++ zynqmp_rpu_no_kernel/src/timer.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
-use jelly_pac::cdns::ttc::Ttc;
+use jelly_pac::cdns::ttc::*;
+
 
 // TTC0 : 0xFF110000 irq:68-70
 // TTC1 : 0xFF120000 irq:71-73
@@ -9,8 +10,18 @@ use jelly_pac::cdns::ttc::Ttc;
 const TTC_ADDRESS: usize = 0xff130000;
 const TTC_INTNO: usize = 74;
 
+
+static TTC: Ttc = Ttc {address: TTC_ADDRESS};
+
+
 // OS用タイマ初期化ルーチン
 pub unsafe fn timer_initialize() {
+    TTC.reset(Timer::Timer1);
+    TTC.reset(Timer::Timer2);
+    TTC.clear_interrupt(Timer::Timer1);
+
+
+    /*
     let ttc = &mut *(TTC_ADDRESS as *mut Ttc);
 
     // タイマ停止
@@ -24,8 +35,35 @@ pub unsafe fn timer_initialize() {
     core::ptr::read_volatile(&mut ttc.interrupt_register_1); // 読み出すとクリア
     core::ptr::write_volatile(&mut ttc.interrupt_register_1, 0x00); // Interrupt : Interval
     core::ptr::write_volatile(&mut ttc.interrupt_enable_1, 0x00); // Interrupt disable
+    */
 }
 
+pub unsafe fn timer_start()
+{
+    // timer1
+    TTC.set_clock_control(Timer::Timer1, ClockControl::PRESCALER_ENABLE, 3);
+    TTC.set_interval_counter(Timer::Timer1, 25000000 - 1); // 1Hz (CPU_1x:100MHz->25MHz)
+    TTC.enable_interrupt(Timer::Timer1, Interrupt::INTERVAL);
+    TTC.set_counter_control(Timer::Timer1, CounterControl::INTERVAL | CounterControl::OUTPUT_WAVEFORM_DISABLE);
+
+    // timer2
+    TTC.set_clock_control(Timer::Timer2,  ClockControl::NONE, 0);
+    TTC.set_counter_control(Timer::Timer2, CounterControl::OUTPUT_WAVEFORM_DISABLE);
+
+ //   core::ptr::write_volatile(&mut ttc.clock_control_2, 0x00); // PS_VAL:1, PS_EN:1
+//    core::ptr::write_volatile(&mut ttc.counter_control_2, 0x20); // start (free run)
+}
+
+pub fn timer_get_counter_value() -> u32 {
+    TTC.get_counter_value(Timer::Timer2)
+}
+
+
+pub fn timer_int_clear() {
+    TTC.clear_interrupt(Timer::Timer1);
+}
+
+/*
 // タイマ動作開始
 pub unsafe fn timer_start() {
     let ttc = &mut *(TTC_ADDRESS as *mut Ttc);
@@ -55,3 +93,4 @@ pub fn timer_int_clear() {
         core::ptr::read_volatile(&mut ttc.interrupt_register_1); // //  割込み要因クリア(読み出すとクリア)
     }
 }
+*/
