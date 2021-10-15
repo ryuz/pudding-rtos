85cab97ad9c39a01a36f4761633d86d6edd48fde
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-10-15 19:21:30 +0900

    update

--- src/context.rs
+++ src/context.rs
@@ -2,19 +2,16 @@ use core::ptr;
 
 
 #[cfg(target_arch = "x86_64")]
-#[macro_use]
 pub mod x86_64;
 #[cfg(target_arch = "x86_64")]
 pub use x86_64::*;
 
 #[cfg(target_arch = "arm")]
-#[macro_use]
 pub mod arm;
 #[cfg(target_arch = "arm")]
 pub use arm::*;
 
 #[cfg(not(any(target_arch = "x86_64", target_arch = "arm")))]
-#[macro_use]
 pub mod dummy;
 #[cfg(not(any(target_arch = "x86_64", target_arch = "arm")))]
 pub use dummy::*;
