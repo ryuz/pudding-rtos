6414fa486911e1c63724939ea0291d26e510fb9a
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-10-15 18:26:55 +0900

    update

--- src/cpu/dummy.rs
+++ src/cpu/dummy.rs
@@ -1,3 +1,4 @@
+
 #[repr(C)]
 pub struct Context {
     pub sp: usize,
@@ -7,29 +8,15 @@ impl Context {
     pub const fn new() -> Self {
         Context { sp: 0 }
     }
+
+    pub (crate) unsafe fn _create(&mut self, _stack: &mut [u8], _entry: extern "C" fn(isize), _exinf: isize) {}
+    pub (crate) unsafe fn _start(&mut self){}
+    pub (crate) unsafe fn _switch(&mut self, _current: &mut Context) {}
 }
 
-pub unsafe fn interrupt_initialize(_stack: &mut [isize]) {}
-
-pub unsafe fn cpu_initialize(_stack: &mut [isize]) {}
-
+pub unsafe fn cpu_initialize() {}
 pub unsafe fn cpu_lock() {}
-
 pub unsafe fn cpu_unlock() {}
-
 pub unsafe fn cpu_halt() {}
 
-#[no_mangle]
-pub extern "C" fn _kernel_context_create(
-    _ctxcb: *mut Context,
-    _isp: usize,
-    _entry: extern "C" fn(isize),
-    _ext: isize,
-) {
-}
-
-#[no_mangle]
-pub extern "C" fn _kernel_context_start(_ctxcb_new: *mut Context) {}
-
-#[no_mangle]
-pub extern "C" fn _kernel_context_switch(_ctxcb_new: *mut Context, _ctxcb_now: *mut Context) {}
+pub unsafe fn interrupt_initialize(_stack: &mut [u8]) {}
