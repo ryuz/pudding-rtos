a6e5d744da197dac8f8ceea9be9c0d24c1c70d59
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-10-16 10:51:13 +0900

    update timeout

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -1,68 +1,48 @@
+use crate::*;
 use crate::system::*;
-use crate::task::*;
 
-pub type SemCount = i32;
 
-// Task control block
-// static初期化の為に泣く泣くすべてpubにする
+// Semephore control block
 pub struct Semaphore {
-    pub queue: TaskQueue,
-    pub count: SemCount,
+    queue: TaskQueue,
+    order: Order,
+    count: SemCount,
 }
 
-/*
-// static初期化時に中身を知らなくてよいようにマクロで補助
-#[macro_export]
-macro_rules! semaphore_default {
-    ($x:expr) => {
-        Semaphore {
-            queue: task_queue_default!(),
-            count: $x,
-        }
-    };
-    () => {
-        Semaphore {
-            queue: task_queue_default!(),
-            count: 0,
-        }
-    };
-}
-*/
 
 impl Semaphore {
-    pub const fn new(init_count: SemCount) -> Self {
+    pub const fn new(init_count: SemCount, order: Order) -> Self {
         Semaphore {
             queue: TaskQueue::new(),
+            order: order,
             count: init_count,
         }
     }
 
     pub fn wait(&mut self) {
-        unsafe {
-            let _sc = SystemCall::new();
-            if self.count > 0 {
-                self.count -= 1;
-            } else {
-                let task = detach_ready_queue().unwrap();
-                self.queue.insert_priority_order(task);
-                set_dispatch_reserve_flag();
-            }
+        let _sc = SystemCall::new();
+        if self.count > 0 {
+            self.count -= 1;
+        } else {
+            let task = current_task().unwrap();
+            task.detach_from_queue();   // remove from ready queue
+            task.attach_to_queue(&mut self.queue, self.order);
+            set_dispatch_reserve_flag();
         }
     }
 
     pub fn signal(&mut self) {
-        unsafe {
-            let _sc = SystemCall::new();
-            let head = self.queue.pop_front();
-            match head {
-                None => {
-                    self.count += 1;
-                }
-                Some(task) => {
-                    task.attach_ready_queue();
-                    set_dispatch_reserve_flag();
-                }
-            };
-        }
+        let _sc = SystemCall::new();
+        let head = self.queue.pop_front();
+        match head {
+            None => {
+                self.count += 1;
+            }
+            Some(task) => {
+                task.attach_to_ready_queue();
+                set_dispatch_reserve_flag();
+            }
+        };
     }
 }
+
