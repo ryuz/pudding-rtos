95518e663d9596de22e4c9b08427c0f1636e9e56
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-10-20 22:17:38 +0900

    update

--- samples/hello/src/main.rs
+++ samples/hello/src/main.rs
@@ -4,19 +4,22 @@ use kernel::*;
 
 
 
+static mut STACK0: [u8; 4096] = [0; 4096];
+static mut STACK1: [u8; 4096] = [0; 4096];
+
 static mut TASK0: Task = Task::new();
 static mut TASK1: Task = Task::new();
 
-static mut STACK0: [u8; 4096] = [0; 4096];
-static mut STACK1: [u8; 4096] = [0; 4096];
+static mut SEM0: Semaphore = Semaphore::new(0, Order::Priority);
+static mut FLG0: Eventflag = Eventflag::new(0);
 
 
 fn main() {
     println!("Start");
     unsafe {
         kernel::initialize();
-        TASK0.create(0, task0, 0, &mut STACK0);
-        TASK1.create(0, task1, 0, &mut STACK1);
+        TASK0.create(0, task0, 2, &mut STACK0);
+        TASK1.create(0, task1, 1, &mut STACK1);
 
         TASK0.activate();
         TASK1.activate();
@@ -24,6 +27,14 @@ fn main() {
     for _ in 0..10 {
         println!("time_tick");
         kernel::supply_time_tick(1);
+    }
+
+    unsafe {
+        println!("FLG0.set_flag(4)");
+        FLG0.set_flag(4);
+
+        println!("SEM0.signal()");
+        SEM0.signal();
     }
 
     println!("End");
@@ -34,9 +45,28 @@ fn task0(_exinf: isize) {
     kernel::sleep(5);
     let t = unsafe { &mut TASK1 };
     t.activate();
+
+    unsafe {
+        println!("SEM0.signal()");
+        SEM0.signal();
+
+        println!("FLG0.set_flag(1)");
+        FLG0.set_flag(1);
+    }
+
     println!("Task0_end");
 }
 
 fn task1(_exinf: isize) {
-    println!("Task1");
+    println!("Task1_start");
+    unsafe {
+        println!("Task1: SEM0.wait() start");
+        SEM0.wait();
+        println!("Task1: SEM0.wait() end");
+
+        println!("Task1: FLG0.wait() start");
+        FLG0.wait(0x5, WaitFlagMode::AndWait);
+        println!("Task1: FLG0.wait() end");
+    }
+    println!("Task1_end");
 }
